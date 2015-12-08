8431aa8b2d5f2d986488af771718eaebb5a2ce62
Henry Han <ruhan@redhat.com>
2015-12-08 19:28:35 +0800

    Able to start a process like './maitai.pl process start -x post -deploymentId com.myorganization.myprojects:test:1.9 -processDefId test.testparams'

--- maitai.pl
+++ maitai.pl
@@ -10,9 +10,43 @@ use Net::SSL;
 use HTTP::Request;
 use HTTP::Cookies;
 use Term::ReadKey;
+use Switch;
 
 $ENV{HTTPS_DEBUG} = 0;
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+
+my $args = {};
+
+my $method = "GET"; # POST/PUT/DELETE
+my $resource = "process"; # process/task/repository/history/deployment, etc.
+my $action = "start"; #start/abort/signal for process; claim/release/start/complete for task
+
+#print Dumper(@ARGV);
+
+while (@ARGV) {
+  $_ = shift @ARGV;
+  switch($_) {
+    case /^-/ {
+      $args->{$_} = $ARGV[0];
+    }
+    case /process|task|repository|history|deployment/ { 
+      $resource = $_;
+    }
+    case /start|abort|signal|claim|release|start|complete/ { 
+      $action = $_;
+    }
+  }
+  shift @ARGV;
+}
+if ($args->{"-x"}) {
+  $method = $args->{"-x"};
+}
+my $deploymentId = $args->{"-deploymentId"};
+my $processDefId = $args->{"-processDefId"};
+my $procInstanceID = $args->{"-procInstanceID"};
+
+print Dumper($args);
+print $resource . " " . $action . "\n";
 
 my $user = '';
 my $pass = '';
@@ -30,6 +64,11 @@ $homeUrl .= 'business-central';
 
 my $url = $homeUrl . '/rest/task/query?potentialOwner=ruhan';
 
+if ($resource eq "process") {
+  $url = $homeUrl . "/rest/runtime/$deploymentId/process/$processDefId/$action";
+  print $url . "\n";
+}
+
 my $agent = LWP::UserAgent->new();
 
 # By default, an LWP::UserAgent object doesn't implement cookies. Making it do so is as simple as this
@@ -38,9 +77,13 @@ $agent->cookie_jar( {} );
 # Access home url to force authentication via Kerberos keytab if run kinit
 my $response = $agent->get( $homeUrl );
 
+my @ns_headers = ('Accept' => 'application/json');
+
 # Issue the request
-$response = $agent->get( $url ); 
-print Dumper($response);
+my $request = HTTP::Request->new(uc $method => $url);
+$response = $agent->request($request, @ns_headers);
+#$response = $agent->get( $url, @ns_headers ); 
+#print Dumper($response);
 
 # Get 401 Unauthorized if not authenticated
 if ($response->{"_rc"} =~ /401/) {
@@ -55,13 +98,13 @@ if ($response->{"_rc"} =~ /401/) {
 
 # Basic Authentication
 if ($basicAuth) {
-  my $request = HTTP::Request->new(GET => $url);
+  my $request = HTTP::Request->new(uc $method => $url);
   $request->authorization_basic($user, $pass);
-  $response = $agent->request($request);
+  $response = $agent->request($request, @ns_headers);
 }
 
 die "Couldn't get $url" unless defined $response;
 die "Error: ", $response->status_line unless $response->is_success;
 
-print $response->content;
+print $response->content . "\n";
 
