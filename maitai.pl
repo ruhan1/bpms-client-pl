fa1fdc414b4210dc12e96c27e7da70257de29824
Henry Han <ruhan@redhat.com>
2016-01-14 18:32:09 +0800

    Do friendly output

--- maitai.pl
+++ maitai.pl
@@ -1,5 +1,5 @@
-#!/usr/bin/env perl
- 
+#!/usr/bin/perl -w
+
 use strict;
 use warnings;
 
@@ -15,6 +15,11 @@ use URI::Escape;
 use XML::Simple;
 use Switch;
 use Config::Simple;
+use Pod::Usage;
+
+if (scalar @ARGV == 0) {
+  pod2usage({-verbose => 2});
+}
 
 $ENV{HTTPS_DEBUG} = 0;
 $ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
@@ -27,10 +32,9 @@ my %deployment_methods = ();
 my $resource = ""; 
 my $action = ""; 
 my $params = "";
-
 my %ns_headers = ();
 
-print Dumper(@ARGV) if defined $ENV{MAITAI_DEBUG};
+print Dumper(@ARGV) if defined $ENV{BPMS_DEBUG};
 
 while (@ARGV) {
   $_ = shift @ARGV;
@@ -66,7 +70,7 @@ while (@ARGV) {
   }
   if ($do_shift) { shift @ARGV; }
 }
-print Dumper(\%ns_headers) if defined $ENV{MAITAI_DEBUG};
+print Dumper(\%ns_headers) if defined $ENV{BPMS_DEBUG};
 
 my $cfg = get_config();
 die "Can not load config file" unless defined $cfg;
@@ -76,17 +80,11 @@ my $processDefId = $args->{"-processDefId"};
 my $procInstanceId = $args->{"-procInstanceId"};
 my $taskId = $args->{"-taskId"};
 my $requestContent;
+my $userpassRef;
 
-#get_latest_version(\$deploymentId); # append the latest version if not specified
-#exit;
-
-print Dumper($args) if defined $ENV{MAITAI_DEBUG};
-print $resource . " " . $action . "\n" if defined $ENV{MAITAI_DEBUG};
-print $params . "\n" if defined $ENV{MAITAI_DEBUG};
-
-my $user = '';
-my $pass = '';
-my $basicAuth = 0;
+print Dumper($args) if defined $ENV{BPMS_DEBUG};
+print $resource . " " . $action . "\n" if defined $ENV{BPMS_DEBUG};
+print $params . "\n" if defined $ENV{BPMS_DEBUG};
 
 my $homeUrl = $ENV{BPMS_HOME};
 if (not $homeUrl) {
@@ -100,8 +98,16 @@ $homeUrl .= 'business-central';
 my $url = '';
 my $method = '';
 
+my $originalDeploymentId;
+if ($deploymentId) {
+  my $version = (split(/:/, $deploymentId))[2];
+  if (not $version) {
+    $originalDeploymentId = $deploymentId; # keep old one, use it when get latest version
+    $deploymentId .= ":[version]";
+  }
+}
+
 if ($resource eq "process") {
-  get_latest_version(\$deploymentId);
   $url = $homeUrl . "/rest/runtime/$deploymentId/process/$processDefId/$action";
   $method = $process_methods{$action} || 'post';
 } elsif ($resource eq "task") {
@@ -115,7 +121,7 @@ if ($resource eq "process") {
   $url = $homeUrl . "/rest/deployment";
   if ($deploymentId) { $url .= "/$deploymentId"; }
   if ($action) { 
-    if ($action =~ /processes/) { # no api, have to exec GetProcessIdsCommand
+    if ($action =~ /processes/) { # no direct api, have to exec GetProcessIdsCommand
       $url = $homeUrl . "/rest/execute";
       $requestContent = "<command-request><get-process-ids/><deployment-id>$deploymentId</deployment-id></command-request>";
     } else {
@@ -126,7 +132,7 @@ if ($resource eq "process") {
     $method = 'get';
   }
 } elsif ($resource eq "conf") {
-  if ($action =~ /(\w+)\=([\w\:\/]+)/) {
+  if ($action =~ /(\w+)\=(.+)/) {
     $cfg->param($1, $2);
     $cfg->save();
   } else {
@@ -137,17 +143,13 @@ if ($resource eq "process") {
   die "$resource not exist!";
 }
 $url .= $params;
-print $url . "\n" if defined $ENV{MAITAI_DEBUG};
+print $url . "\n" if defined $ENV{BPMS_DEBUG};
 
-my $agent = LWP::UserAgent->new();
+if ($url =~ /:\[version\]/) { # process version not specified
+  $url = get_complete_url();
+}
 
-# By default, LWP::UserAgent object doesn't implement cookies. Making it do so is as simple as this
-$agent->cookie_jar( {} );
-
-# Access homeUrl to force Kerberos authentication if use kinit
-my $response = $agent->get( $homeUrl );
-
-# Send the request
+my $agent = get_agent();
 my $request = HTTP::Request->new(uc $method => $url);
 if ($requestContent) {
   $request->content($requestContent);
@@ -158,28 +160,9 @@ while (my ($key, $value) = each (%ns_headers)) {
   $request->header($key => $value);
 }
 
-print Dumper($request) if defined $ENV{MAITAI_DEBUG};
-
-$response = $agent->request($request);
-print Dumper($response) if defined $ENV{MAITAI_DEBUG};
-
-# Get 401 Unauthorized if not authenticated
-if ($response->{"_rc"} =~ /401/) {
-  print "Enter username: ";
-  chomp ($user = <STDIN>);
-  print "Enter password: ";
-  ReadMode('noecho'); # don't echo
-  chomp($pass = <STDIN>);
-  ReadMode(0);        # back to normal
-  $basicAuth = 1;
-  print "\n";
-}
-
-# Basic Authentication
-if ($basicAuth) {
-  $request->authorization_basic($user, $pass);
-  $response = $agent->request($request);
-}
+print Dumper($request) if defined $ENV{BPMS_DEBUG};
+my $response = do_request($request, $agent);
+print Dumper($response) if defined $ENV{BPMS_DEBUG};
 
 die "Couldn't get $url" unless defined $response;
 die "Error: ", $response->status_line unless $response->is_success;
@@ -188,7 +171,55 @@ print_friendly($response->content, $resource, $action);
 
 
 
-### functions
+### functions ###
+
+sub get_complete_url {
+  my $agent = get_agent();
+  my $request = HTTP::Request->new(GET => $homeUrl . "/rest/deployment");
+  my $response = do_request($request, $agent);
+  print Dumper($response) if defined $ENV{BPMS_DEBUG};
+  my $hash_ref = get_deployment_hash_ref($response->content);
+  my $version = $hash_ref->{$originalDeploymentId};
+  $url =~ s/\[version\]/$version/;
+  print $url . "\n" if defined $ENV{BPMS_DEBUG};
+  return $url;
+}
+
+sub do_request {
+  my ($request, $agent) = @_;
+  my $response = $agent->request($request);
+  if ($response->{"_rc"} =~ /401/) {
+    if (not $userpassRef) {
+      $userpassRef = prompt_for_userpasswd();
+    }
+    $request->authorization_basic(@ $userpassRef);
+    $response = $agent->request($request); # do it again
+  }
+  return $response;
+}
+
+sub prompt_for_userpasswd {
+  my $arrayref = [];
+  print "Enter username: ";
+  chomp (my $user = <STDIN>);
+  print "Enter password: ";
+  ReadMode('noecho'); # don't echo
+  chomp(my $pass = <STDIN>);
+  ReadMode(0); # back to normal
+  print "\n";
+  $$arrayref[0] = $user;
+  $$arrayref[1] = $pass;
+  return $arrayref;
+}
+
+sub get_agent {
+  my $ua = LWP::UserAgent->new();
+  # By default, LWP::UserAgent doesn't implement cookies. Making it do so is as simple as this
+  $ua->cookie_jar( {} ); 
+  # Access homeUrl to force Kerberos authentication if use kinit
+  $ua->get( $homeUrl );
+  return $ua; 
+}
 
 sub get_default_homeUrl {
   return "https://maitai-bpms.host.dev.eng.pek2.redhat.com";
@@ -203,38 +234,20 @@ sub get_config {
     print $fh "homeUrl=" . get_default_homeUrl() . "\n";
     close $fh;
   }
-  return new Config::Simple("$configfile");
-}
-
-sub apply_latest_version {
-  my $deploymentIdRef = shift;
-  my $deploymentId = ${$deploymentIdRef};
-  my @gav = split(/:/, $deploymentId);
-  if (scalar @gav == 3) { 
-    return; 
-  }
-  my $version = get_latest_version($deploymentId);
-  ${$deploymentIdRef} = $deploymentId . $version;
-}
-
-# TODO 
-sub get_latest_version {
+  return new Config::Simple($configfile);
 }
 
 sub print_friendly {
   my ($content, $resource, $action) = @_;
-  if ($resource eq "process") {
-    print $content . "\n";
-  } elsif ($resource eq "task") {
-    if ($action eq "query") {
-      print $content . "\n";
-    } else {
-      print $content . "\n";
-    }
-  } elsif ($resource eq "deployment") {
+  if ($resource eq "deployment") {
     print_deployment($content, $deploymentId, $action);
   } else {
-    print $content . "\n";
+    my $config = eval { XMLin($content) };
+    if($@) { # parse error
+      print $content . "\n";
+    } else {
+      print Data::Dumper->Dump( [ $config ], [ qw(*result) ] );
+    }
   }
 }
 
@@ -252,45 +265,62 @@ sub print_deployment {
 }
 
 sub print_deployment_processes {
-  my $config = XMLin(shift);
+  my $config = XMLin(shift, ForceArray => [ 'string' ]);
   foreach my $str (@ { $config->{"string-list"}->{"string"} }) {
     print "$str\n";
   }
 }
 
 sub print_deployment_all {
-  my $config = XMLin(shift);
-  my $hash_ref = {};
-  foreach my $unit (@ { $config->{"deployment-unit"} }) {
-    my $key = $unit->{"groupId"} . ":" . $unit->{"artifactId"};
-    if (my $ver = $hash_ref->{ $key }) {
-      if ($unit->{"version"} lt $ver) { # version less than existing version
-        next;
-      }
-    }
-    $hash_ref->{ $key } = $unit->{"version"};
-  }
+  my $hash_ref = get_deployment_hash_ref(shift);
   while (my ($key, $value) = each (%$hash_ref)) {
     print $key . ":" . $value . "\n";
   }
 }
 
+sub get_deployment_hash_ref {
+  my $config = XMLin(shift);
+  my $hash_ref = {};
+  foreach my $unit (@ { $config->{"deployment-unit"} }) {
+    my $key = $unit->{"groupId"} . ":" . $unit->{"artifactId"};
+    if (my $ver = $hash_ref->{ $key }) {
+      if ($unit->{"version"} lt $ver) { # skip version less than existing version
+        next;
+      }
+    }
+    $hash_ref->{ $key } = $unit->{"version"};
+  }
+  return $hash_ref;
+}
+
+
+=pod
 
 =head1 NAME
 
-BPMS 6.x client tool
+Maitai is jBPM/BPMS 6.x command tool, developed by Perl
 
 =head1 SYNOPSIS
 
-./maitai.pl <resource> <action> [parameters...]
+maitai <resource> <action> [parameters...]
 
 =head1 DESCRIPTION
 
-This is a tool to access BPMS 6.x via REST APIs. It can use either BASIC or Kerberos (kinit) authentication. It supports basic process and task operations, such as start a process, complete a task, etc. 
+Maitai is a tool to access jBPM/BPMS 6.x via REST APIs. It can use either BASIC or Kerberos (kinit) authentication. It supports process and task operations, such as start process, complete task, etc. This program uses system variable BPMS_HOME to identify the target server. You can also use "./maitai conf homeUrl=http://host[:port]" to specify the Url. It will be remembered in ~/.maitai/config. 
 
-For example, to start a process "./maitai.pl process start -deploymentId com.myorganization.myprojects:test:1.9 -processDefId test.testparams -d aString='Hello,world!' -d aInt=200i -d aLong=30"; to query my tasks "./maitai.pl task query -D potentialOwner=ruhan". 
+For beginners, you may want to list all deployments and find the right process by:
+  ./maitai deployment                                   # this will list all deployments
+  ./maitai deployment processes -deploymentId <id>      # this will list all processes in a certain deployment
 
-This program uses system varible BPMS_HOME (if exists) to identify the target server. Also you can use "./maitai.pl conf homeUrl=http://host[:port]" to specify the Url (it will be saved in ~/.maitai/config and you only need to do it once). 
+Start a process: 
+  ./maitai process start -deploymentId <id> -processDefId <pid> [-d parameters...]
+
+Query my tasks:
+  ./maitai task query -D potentialOwner=<uid>
+
+For example:
+  ./maitai process start -deploymentId draft:test:1.0 -processDefId test.testparams -d aString='Hello,world!' -d aInt=200i -d aLong=30
+  ./maitai task query -D potentialOwner=ruhan
 
 =head2 Resources
 
@@ -298,7 +328,7 @@ process/task/history/deployment
 
 =head2 Actions
 
-start/abort/signal for process; query/claim/release/start/complete for task; list for deployment. 
+start/abort/signal for process; query/claim/release/start/complete for task; processes for deployment. 
 
 =head2 Parameters
 
@@ -306,7 +336,7 @@ start/abort/signal for process; query/claim/release/start/complete for task; lis
 
 =item C<-deploymentId>
 
-Artifact deployment id, such as "com.myorganization.myprojects:test:1.9"
+Project deployment id of format "groupId:artifactId:version", such as "draft:test:1.0"
 
 =item C<-processDefId>
 
@@ -318,7 +348,7 @@ Task id. User can find all tasks via task query action.
 
 =item C<-d>
 
-Specify process or task input arguments, e.g, "-d aString='Hello,world!'". For integer, append "i" to the number, e.g, "-d aInt=200i" (by default a number will be converted to a Long). For boolean, use true or false, e.g, "-d aBoolean=true". 
+Specify process or task input arguments, e.g, "-d aString='Hello,world!'". For integer, append "i" to the number, e.g, "-d aInt=200i" (by default number will be converted to Long). For boolean, use true or false, e.g, "-d aBoolean=true". 
 
 =item C<-D>
 
